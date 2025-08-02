e3975cbedf9e496ba4bbda02e536b959464f19c9
Prefrontal Corporate <prefrontalcorporate@gmail.com>
2025-08-02 06:19:03 +0000

    web: silence dev CORS warning

--- app/web/next.config.js
+++ app/web/next.config.js
@@ -10,6 +10,7 @@ const nextConfig = {
    */
   allowedDevOrigins: [
     'http://34.134.149.61:3000',  // VM external IP
+    'http://34.134.149.61',
     'http://localhost:3000',
     'http://127.0.0.1:3000',
   ],
